a0062795fcefd7b9666da0b04cd0e7179d67c999
SudeepRed <sudeep19313@iiitd.ac.in>
2023-04-05 13:25:49 +0530

    update test

--- app/test_contact_form.py
+++ app/test_contact_form.py
@@ -2,7 +2,7 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 
-def run(playwright: Playwright) -> None:
+def test_contact_form(playwright: Playwright):
     browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     page = context.new_page()
@@ -15,12 +15,15 @@ def run(playwright: Playwright) -> None:
     page.get_by_role("link", name="Contact").click()
     page.locator("input[name=\"name\"]").click()
     page.locator("input[name=\"name\"]").fill("test")
+    expect(page.locator("input[name=\"name\"]")).to_have_value("test")
     page.locator("input[name=\"email\"]").click()
     page.locator("input[name=\"email\"]").fill("test@test.com")
+    expect(page.locator("input[name=\"email\"]")).to_have_value("test@test.com")
     page.get_by_role("spinbutton").click()
     page.get_by_role("spinbutton").fill("123")
     page.locator("textarea[name=\"message\"]").click()
     page.locator("textarea[name=\"message\"]").fill("123")
+    expect(page.locator("textarea[name=\"message\"]")).to_have_value("123")
     page.get_by_role("button", name="Submit").click()
 
     # ---------------------
@@ -29,4 +32,4 @@ def run(playwright: Playwright) -> None:
 
 
 with sync_playwright() as playwright:
-    run(playwright)
+    test_contact_form(playwright)
