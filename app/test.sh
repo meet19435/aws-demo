7b9e82e637f7b0729a244bf908da1004f0d696ab
SudeepRed <sudeep19313@iiitd.ac.in>
2023-04-05 07:28:30 +0530

    add config

--- app/test.sh
+++ app/test.sh
@@ -2,11 +2,4 @@
 
 # Get the container ID
 CONTAINER_ID=$(docker ps -aqf "name=project-pythonapp")
-
-# Get the container IP address
-CONTAINER_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_ID)
-
-# Display the environment variable
-docker exec $CONTAINER_ID sh -c "echo \"$CONTAINER_IP\" > container_ip.txt"
-docker exec $CONTAINER_ID cat container_ip.txt
 docker exec $CONTAINER_ID poetry run pytest
--- app/test_contact_form.py
+++ app/test_contact_form.py
@@ -6,13 +6,7 @@ def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     page = context.new_page()
-    print(page)
-    f = open("container_ip.txt", "r")
-    DOCKER_FLASK_IP = f.read()
-    f.close()
-    DOCKER_FLASK_IP = DOCKER_FLASK_IP.strip()
-    print(DOCKER_FLASK_IP)
-    ip ="http://"+DOCKER_FLASK_IP+":8080/"
+    ip ="http://localhost:8080/"
     print(ip)
     page.goto(ip)
     page.get_by_role("link", name="Contact").click()
