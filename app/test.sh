d5e020d810e251b3ac926076ab9465c745f6fd6f
SudeepRed <sudeep19313@iiitd.ac.in>
2023-04-05 05:43:28 +0530

    fix project name

--- app/test.sh
+++ app/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get the container ID
-CONTAINER_ID=$(sudo docker ps -aqf "name=aws-demo_pythonapp")
+CONTAINER_ID=$(sudo docker ps -aqf "name=project-pythonapp")
 
 # Get the container IP address
 CONTAINER_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_ID)
@@ -11,4 +11,4 @@ export DOCKER_FLASK_IP="$CONTAINER_IP"
 
 # Display the environment variable
 echo "$CONTAINER_IP" > container_ip.txt
-sudo docker exec $(sudo docker ps -aqf "name=aws-demo_pythonapp") poetry run pytest
+sudo docker exec $(sudo docker ps -aqf "name=project-pythonapp") poetry run pytest
