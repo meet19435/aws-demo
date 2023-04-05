cee561b73beab9f2b54ca1d48910e75a1abfe625
SudeepRed <sudeep19313@iiitd.ac.in>
2023-04-05 13:12:57 +0530

    update test.sh

--- app/test.sh
+++ app/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get the container ID
-CONTAINER_ID=$(docker ps -aqf "name=aws-demo_pythonapp")
+CONTAINER_ID=$(docker ps -aqf "name=project-pythonapp")
 CONTAINER_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_ID)
 
 echo "$CONTAINER_IP" > container_ip.txt
